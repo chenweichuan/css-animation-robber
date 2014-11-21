3bb8deec95c0838374a28852aacd847586ec6e13
chenweichuan <stream_26@sina.com>
2014-11-21 17:30:41 +0800

    add refresh animation btn

--- scripts/index.php
+++ scripts/index.php
@@ -39,6 +39,14 @@
         .preview-animation-item .preview-animation-code {
             width: 100%;
             height:50px;
+            overflow: hidden;
+        }
+        .refresh-btn {
+            position: fixed;
+            top:50px;
+            right:20px;
+            font-size:30px;
+            color: #dd0000;
         }
     </style>
 
@@ -53,12 +61,12 @@
         <br/>
 
         <div class="clearfix">
-            <label style="float:left;"><span style="color:red;">*</span>&nbsp;HTML5地址</label>
+            <label style="float:left;"><span style="color:red;">*</span>&nbsp;HTML5地址&nbsp;</label>
             <div style="float:left;">
-                <input id="url" type="text" name="url" value="http://m.jobtong.com/e/1024/power" placeholder="请输入HTML5 URL" style="width:800px;"/>
+                <input id="url" type="text" name="url" value="http://m.jobtong.com/e/1024/power" placeholder="请输入HTML5 URL" style="width:800px;"/>&nbsp;
             </div>
             <div style="float:left;">
-                <input class="js-submit-btn" type="button" value="提取CSS"/>
+                <input class="js-submit-btn" type="button" value="提取CSS"/>&nbsp;
             </div>
         </div>
 
@@ -66,6 +74,8 @@
 
         <div id="preview" class="clearfix preview"></div>
     </div>
+
+    <input class="refresh-btn js-refresh-btn" type="button" value="重播动画"/>
 
     <script>
         $( ".js-submit-btn" ).click( function() {
@@ -84,13 +94,28 @@
                 $btn.attr( "disabled", false ).val( btnText );
             } ) : ( alert( "请输入HTML5地址" ), $btn.attr( "disabled", false ).val( btnText ) );
         } );
-        setInterval( function() {
+        var autoRefreshTimer,
+            initAutoRefresh = function() {
+                autoTimer = setInterval( function() {
+                    $( "#preview" ).find( ".preview-animation-element > .not-infinite" ).hide();
+                    setTimeout( function() {
+                        $( "#preview" ).find( ".preview-animation-element > .not-infinite" ).show();
+                    }, 100 );
+                }, 8000 );
+            };
+        $( ".js-refresh-btn" ).click( function() {
+            var $btn  = $( this );
+            clearInterval( autoRefreshTimer );
+            $btn.attr( "disabled", true );
             $( "#preview" ).find( ".preview-animation-element > .not-infinite" ).hide();
             setTimeout( function() {
                 $( "#preview" ).find( ".preview-animation-element > .not-infinite" ).show();
             }, 100 );
-        }, 3000 );
+            setTimeout( function() {
+                $btn.attr( "disabled", false );
+            }, 2000 );
+        } );
+        initAutoRefresh();
     </script>
-
   </body>
 </html>
